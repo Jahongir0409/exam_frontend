6bf0139fd52fee45ca0ebd9b5bb9c1e0e208d39f
Jahongir0409 <jahongirmamanzarov1@gmail.com>
2021-05-03 00:45:50 +0500

    js adding

--- js/admin.js
+++ js/admin.js
@@ -1,2 +1,19 @@
-let i = 'Jahongir'
-console.log(i)
+let formAdd = document.querySelector('#formAdd')
+let inputImg =  document.querySelector('#inputImg')
+let inputTitle = document.querySelector('#inputTitle')
+let inputVideo = document.querySelector('#inputVideo')
+formAdd.onsubmit = function (event) {
+	event.preventDefault()
+	let obj = {
+		title : inputTitle.value,
+		img : inputImg.value,
+		sourceLink : inputVideo.value
+	}
+	films.push(obj)
+	window.localStorage.setItem('data',JSON.stringify(films))
+
+	inputTitle.value = null
+	inputImg.value = null
+	inputVideo.value = null
+}
+console.log(films)
